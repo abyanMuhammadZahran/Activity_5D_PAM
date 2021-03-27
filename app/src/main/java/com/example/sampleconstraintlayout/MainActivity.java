1d89c2ca0b33c00fbac88ceb738166d7bd9ac424
AbyanMuhammadZahran <zahraan.abyan@gmail.com>
2021-03-27 17:25:12 +0700

    Memperbaiki DaftarActivity.java

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sampleconstraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,31 +48,93 @@ public class MainActivity extends AppCompatActivity {
                 //Menyimpan input user di editText password kedalam variable password
                 password = edpassoword.getText().toString();
 
+                //Set Email Yang benar
+                String email = "admin@gmail.com";
+                //Set Password Yang benar
+                String pass = "123";
+
+                // mengecek apakah isi dari email dan password sudah sama dengan email dan password
+                //yang di set
+
+                if (nama.equals(email) && password.equals(pass)) {
+                    //Membuat variable Toast dan Menampilkan Pesan "Login Sukses"
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "LoginSukses",
+                            Toast.LENGTH_LONG);
+
+                    //Mnampilkan Toast
+                    t.show();
+
+                    //Membuat Object Bundle
+                    Bundle b = new Bundle();
+
+                    //masukan value dari variable nama dengan kunci "a"
+                    //dan dimasukan kedalam bundle
+                    b.putString("a", nama.trim());
+
+                    //masukan value dari variable nama dengan kunci "a"
+                    //dan dimasukan kedalam bundle
+                    b.putString("b", password.trim());
+
+                    //membuat objek intent berpindah activity dari mainactivity ke Activity Hasil
+                    Intent i = new Intent(getApplicationContext(), ActivityKedua.class);
+
+                    i.putExtras(b);
+
+                    startActivity(i);
+                } else{
+                    //membuat variable toast dan membuat toast login gagal
+
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "login Gagal", Toast.LENGTH_LONG);
+
+                    t.show();
+                }
+
 //                //Membuat Variable Toast dan membuat toast dengan menambahkan variable nama dan password
 //                Toast t = Toast.makeText(getApplicationContext(),
 //                        "email anda: "+nama+" dan password anda: "+password+"", Toast.LENGTH_LONG);
 //                //Menampilkan Toast
 //                t.show();
-                Validasi();
+//                Validasi();
             }
         });
 
     }
 
-    public void Validasi(){
-        if (nama.equals("admin") && password.equals("password")) {
-            Toast.makeText(this,"Login Berhasil", Toast.LENGTH_SHORT).show();
-
-        }else if (nama!=("admin")){
-            Toast.makeText(this,"Email Salah", Toast.LENGTH_SHORT).show();
-            return;
-        }else if (password!=("password")){
-            Toast.makeText(this,"Password Salah", Toast.LENGTH_SHORT).show();
-            return;
-        } else {
-            Toast.makeText(this, "Email Dan Password Salah", Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah mnDaftar
+        if( item.getItemId()  == R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "Daftar Activity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
         }
+        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk Menampilkan Menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    public void Validasi(){
+//        if (nama.equals("admin") && password.equals("password")) {
+//            Toast.makeText(this,"Login Berhasil", Toast.LENGTH_SHORT).show();
+//
+//        }else if (nama!=("admin")){
+//            Toast.makeText(this,"Email Salah", Toast.LENGTH_SHORT).show();
+//            return;
+//        }else if (password!=("password")){
+//            Toast.makeText(this,"Password Salah", Toast.LENGTH_SHORT).show();
+//            return;
+//        } else {
+//            Toast.makeText(this, "Email Dan Password Salah", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
 
 }
